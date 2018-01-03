f4b90dcb39e57f458b2676d7dd3b92d63f17405d
Sean Sutton <sutton_mjbc@icloud.com>
2018-01-03 10:52:36 -0500

    Initial Commit

---
+++ Ryde/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ryde
+//
+//  Created by Sean Sutton on 1/3/18.
+//  Copyright © 2018 Au Development. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
